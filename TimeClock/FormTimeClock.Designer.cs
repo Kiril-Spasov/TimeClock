cd104a634179cc1a8b9f49fdaf95acf74bded701
Kiril Spasov <spasov.kiril@gmail.com>
2022-11-23 12:21:17 +0200

    Add project files.

---
+++ TimeClock/FormTimeClock.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace TimeClock
+{
+    partial class FormTimeClock
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnCalculate = new System.Windows.Forms.Button();
+            this.TxtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BtnCalculate
+            // 
+            this.BtnCalculate.Location = new System.Drawing.Point(42, 65);
+            this.BtnCalculate.Name = "BtnCalculate";
+            this.BtnCalculate.Size = new System.Drawing.Size(200, 66);
+            this.BtnCalculate.TabIndex = 0;
+            this.BtnCalculate.Text = "Calculate Time";
+            this.BtnCalculate.UseVisualStyleBackColor = true;
+            this.BtnCalculate.Click += new System.EventHandler(this.BtnCalculate_Click);
+            // 
+            // TxtResult
+            // 
+            this.TxtResult.Location = new System.Drawing.Point(327, 65);
+            this.TxtResult.Multiline = true;
+            this.TxtResult.Name = "TxtResult";
+            this.TxtResult.Size = new System.Drawing.Size(414, 345);
+            this.TxtResult.TabIndex = 1;
+            // 
+            // FormTimeClock
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TxtResult);
+            this.Controls.Add(this.BtnCalculate);
+            this.Name = "FormTimeClock";
+            this.Text = "Time Clock";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnCalculate;
+        private System.Windows.Forms.TextBox TxtResult;
+    }
+}
+
---
+++ TimeClock/FormTimeClock.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace TimeClock
+{
+    public partial class FormTimeClock : Form
+    {
+        public FormTimeClock()
+        {
+            InitializeComponent();
+        }
+
+
+        int totalTimeInMins = 0;
+
+        private void BtnCalculate_Click(object sender, EventArgs e)
+        {
+            string line1 = "";
+            string line2 = "";
+
+            string path = Application.StartupPath + @"\clock.txt";
+            StreamReader streamReader = new StreamReader(path);
+
+            bool finished = false;
+
+            while (!finished)
+            {
+                line1 = streamReader.ReadLine();
+                line2 = streamReader.ReadLine();
+
+                if (line1 == null || line2 == null)
+                {
+                    finished = true;
+                }
+                else
+                {
+                    CalculateTime(line1, line2);
+                    Display();
+                }
+            }
+        }
+
+        private void CalculateTime(string time1, string time2)
+        {
+
+            int hour1 = Convert.ToInt32(time1.Substring(0, 2));
+            int hour2 = Convert.ToInt32(time2.Substring(0, 2));
+            int mins1 = Convert.ToInt32(time1.Substring(3, 2));
+            int mins2 = Convert.ToInt32(time2.Substring(3, 2));
+
+            //Convert time in mins.
+            int time1InMins = 0;
+            int time2InMins = 0;
+
+            time1InMins = hour1 * 60 + mins1;
+            time2InMins = hour2 * 60 + mins2;
+
+            //For example if the first time is 09:13 and second 04:42.
+            //We need to add 12h to second time to make it 16:42.
+            if (hour1 > hour2)
+            {
+                totalTimeInMins = (time2InMins + 12 * 60) - time1InMins;
+            }
+            else
+            {
+                totalTimeInMins = time2InMins - time1InMins;
+            }
+
+        }
+
+        private void Display()
+        {
+            string result = "";
+
+            int hours = totalTimeInMins / 60;
+            int mins = totalTimeInMins % 60;
+
+            if (totalTimeInMins > 480)
+            {
+                hours = 8;
+                mins = 0;
+            }
+
+            result += hours < 10 ? "0" + hours.ToString() : hours.ToString();
+
+            result += ":";
+
+            result += mins < 10 ? "0" + mins.ToString() : mins.ToString();
+
+            TxtResult.Text +=  result + Environment.NewLine;
+        }
+    }
+}
